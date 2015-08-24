90e5f975a2447286dc5202a2cc2896fe7660a992
Rodrigo Morais <morais.rm@gmail.com>
2015-08-24 10:16:25 -0300

    Cleaning the code.

--- Program.cs
+++ Program.cs
@@ -5,53 +5,7 @@ using System.Linq;
 public class Frog
 {
     enum Way { step = 1, jump = 2 };
-
-    private static int getWay(Way way, int n, int distance, ref string path)
-    {
-        var ways = 0;
-        path = path + "-" + way;
-        distance = distance + (int)way;
-
-        while (distance < n)
-        {
-            if (distance > n)
-            {
-                if (way == Way.jump)
-                {
-                    distance = distance - (int)way;
-                    way = Way.step;
-                    distance = distance + (int)way;
-                }
-
-                if (distance > n)
-                {
-                    ways += 0;
-                }
-                else
-                {
-                    ways += getWay(way, n, distance, ref path);
-                }
-            }
-            else
-            {
-                var returned = getWay(way, n, distance, ref path);
-                if(returned == 1){
-                    ways += returned;
-                    distance = distance - (int)way;
-                    way = way == Way.step ? Way.jump : Way.step;
-                    distance = distance + (int)way;
-                }
-            }
-        }
-
-        if (distance == n)
-        {
-            ways += 1;
-        }
-
-        return ways;
-    }
-
+    
     private static bool sameWay(Way way, int n)
     {
         var distance = 0;
@@ -95,9 +49,6 @@ public class Frog
 
         ways += differentWay(Way.step, n) ? 1 : 0;
         ways += differentWay(Way.jump, n) ? 1 : 0;
-
-        var path = "";
-        var x = getWay(Way.step, n, 0, ref path);
                 
         return ways;
     }
